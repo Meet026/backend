d1e263d3bd7f0cca5591cf057271c4b659bc79c9
Meet <meetpokal04@gmail.com>
2024-03-18 21:28:07 +0530

    all setup configered

--- src/app.js
+++ src/app.js
@@ -9,10 +9,17 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({ extended: true, limit: "16kb"}));
-app.use(express.static("public"));
-app.use(cookieParser)
+// app.use(express.json({limit: "16kb"}));
+// app.use(express.urlencoded({ extended: true, limit: "16kb"}));
+// app.use(express.static("public"));
+// app.use(cookieParser)
+
+// routes
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("api/v1/users", userRouter)
 
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // require('dotenv').config()
-import express from "express";
-const app = express();
+// import express from "express";
+// const app = express();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js"
 
 dotenv.config({
     path: './env'
@@ -15,6 +16,8 @@ connectDB()
     })
 })
 .catch((err) => console.log("MongoDB connection failed !!! ",err))
+
+
 
 
 
